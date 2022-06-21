390ebad1328927c9aeb6257f2392c7cbd5d71a61
Awais Amjed <awaisamjed171@gmail.com>
2022-06-21 20:19:47 +0500

    Update data type of time in User table

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_21_114939) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_21_150851) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "recipes", force: :cascade do |t|
     t.string "name"
-    t.integer "preparation_time"
-    t.integer "cooking_time"
+    t.decimal "preparation_time"
+    t.decimal "cooking_time"
     t.text "description"
     t.boolean "public"
     t.bigint "user_id", null: false
