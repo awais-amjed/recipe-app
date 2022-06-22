54f1687c3e0b1a0d0a84b89b55f083c37c0a51cf
Roshan Bajgain <roshan.bazgain@gmail.com>
2022-06-22 18:50:17 +0545

    Fix linter error

--- app/controllers/public_recipes_controller.rb
+++ app/controllers/public_recipes_controller.rb
@@ -1,14 +1,14 @@
 class PublicRecipesController < ApplicationController
-    def index
-      @public_recipes = Recipe.where(public: true).order(created_at: :desc).map do |public_recipe|
-        {
-          id: public_recipe.id,
-          name: public_recipe.name,
-          user: public_recipe.name,
-          description: public_recipe.description,
-          food_items: public_recipe.recipe_foods.count,
-          total_price: public_recipe.foods.map(&:price).sum
-        }
-      end
+  def index
+    @public_recipes = Recipe.where(public: true).order(created_at: :desc).map do |public_recipe|
+      {
+        id: public_recipe.id,
+        name: public_recipe.name,
+        user: public_recipe.name,
+        description: public_recipe.description,
+        food_items: public_recipe.recipe_foods.count,
+        total_price: public_recipe.foods.map(&:price).sum
+      }
     end
   end
+end
